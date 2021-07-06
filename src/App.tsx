6f68ce98d09e2e4dae08a739f73c4bba761b05d0
muhammad yousuf <muhammadyousuf327@gmail.com>
2021-07-06 22:19:50 +0500

    state mangement in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoList from './component/TodoList'
 import NewTodo from './component/NewTodo';
+import { Todo } from "./todo.model";
 const App: React.FC = () => {
-  const todos = [{ id: "1", text: "work" }];
+  const [todos, setTodes] = useState<Todo[]>([]);
  
   const addTodoHandler = (text: string) => {
     console.log("text", text)
+    setTodes([{ id: Math.random().toString(), text }])
   }
 
   return (
